a76ed1a2bc0ceefa01296c17e7d9044e275a6ae5
Gunther Schulz <dev@guntherschulz.de>
2024-08-10 23:04:40 +0200

    comapre pkgrel as welll

--- check.py
+++ check.py
@@ -15,27 +15,39 @@ def get_latest_version(url):
         else:
             raise ValueError("Unable to extract version from Content-Disposition header")
 
+def get_local_pkgbuild_info():
+    with open('PKGBUILD', 'r') as file:
+        content = file.read()
+    version_match = re.search(r'pkgver=(\d+\.\d+\.\d+)', content)
+    rel_match = re.search(r'pkgrel=(\d+)', content)
+    if version_match and rel_match:
+        return version_match.group(1), rel_match.group(1)
+    else:
+        raise ValueError("Unable to find current version or release in local PKGBUILD")
+
 def get_aur_version(url):
     response = requests.get(url)
     response.raise_for_status()
     pkgbuild_content = response.text
     version_match = re.search(r'pkgver=(\d+\.\d+\.\d+)', pkgbuild_content)
-    if version_match:
-        return version_match.group(1)
+    rel_match = re.search(r'pkgrel=(\d+)', pkgbuild_content)
+    if version_match and rel_match:
+        return version_match.group(1), rel_match.group(1)
     else:
-        raise ValueError("Unable to find current version in AUR PKGBUILD")
+        raise ValueError("Unable to find current version or release in AUR PKGBUILD")
 
 url = 'https://downloader.cursor.sh/linux/appImage/x64'
 aur_url = 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cursor-bin'
 
 try:
     latest_version = get_latest_version(url)
-    aur_version = get_aur_version(aur_url)
+    aur_version, aur_rel = get_aur_version(aur_url)
+    local_version, local_rel = get_local_pkgbuild_info()
 except Exception as e:
     print(f"Error: {str(e)}")
     exit(1)
 
-if latest_version != aur_version:
+if latest_version != aur_version or int(local_rel) < int(aur_rel):
     print(f'update_needed=true')
     print(f'latest_version={latest_version}')
 else:
