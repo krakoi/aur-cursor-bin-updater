e18808be377c01af460076f26a2bd196e69bc41c
Gunther Schulz <dev@guntherschulz.de>
2024-08-15 12:30:01 +0200

    update how debug out put is handled

--- check.py
+++ check.py
@@ -2,6 +2,7 @@
 import requests
 import re
 from packaging import version
+import sys
 
 def get_latest_version(url):
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'}
@@ -49,17 +50,17 @@ except Exception as e:
     exit(1)
 
 # Compare versions and release numbers
-print(f"Debug: latest_version={latest_version}, aur_version={aur_version}, local_rel={local_rel}, aur_rel={aur_rel}")
+print(f"Debug: latest_version={latest_version}, aur_version={aur_version}, local_rel={local_rel}, aur_rel={aur_rel}", file=sys.stderr)
 
 if version.parse(latest_version) > version.parse(aur_version) or int(local_rel) > int(aur_rel):
-    print(f"Debug: Update needed. Reason: {'New version' if version.parse(latest_version) > version.parse(aur_version) else 'Higher local release'}")
+    print(f"Debug: Update needed. Reason: {'New version' if version.parse(latest_version) > version.parse(aur_version) else 'Higher local release'}", file=sys.stderr)
     # If the latest version is newer than the AUR version,
     # or if the local release number is higher than the AUR release number
-    print(f'update_needed=true')
-    print(f'latest_version={latest_version}')
+    print(f'::set-output name=update_needed::true')
+    print(f'::set-output name=latest_version::{latest_version}')
 else:
-    print("Debug: No update needed.")
+    print("Debug: No update needed.", file=sys.stderr)
     # If the AUR version is up-to-date and the local release number is not higher
-    print(f'update_needed=false')
+    print(f'::set-output name=update_needed::false')
 
-print(f"Debug: Comparison results: latest > aur = {version.parse(latest_version) > version.parse(aur_version)}, local_rel > aur_rel = {int(local_rel) > int(aur_rel)}")
+print(f"Debug: Comparison results: latest > aur = {version.parse(latest_version) > version.parse(aur_version)}, local_rel > aur_rel = {int(local_rel) > int(aur_rel)}", file=sys.stderr)
